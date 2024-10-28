e8f3a8738b25a123ea1f4c1e478aadcce6a60539
wianrossouw <wianrossouw@gmail.com>
2024-10-28 21:51:12 +0200

    fix: link update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './sales.html', './why-choose-us.html', './contact-us.html'],  theme: {
+  content: ['./wian-test-2024-10-27/index.html', './sales.html', './wian-test-2024-10-27/why-choose-us.html', './wian-test-2024-10-27/contact-us.html'],  theme: {
     extend: {},
   },
   plugins: [],
